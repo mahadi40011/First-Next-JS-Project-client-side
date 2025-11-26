8c1ab3602e8df63ead4438622e7c213bba67c3de
mahadi40011 <mahadi40011@gmail.com>
2025-11-26 15:05:02 +0600

    Created context folder and 2 file. AuthContext and AuthProvider

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "@/firebase/firebase.config";
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUserProfile = (displayName, photoURL) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, {displayName, photoURL})
+  }
+
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOutUser = () => {
+    setLoading(true);
+    return signOut(auth)
+  }
+
+  const LoginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    setUser,
+    googleLogin,
+    logOutUser,
+    createUser,
+    LoginUser,
+    updateUserProfile,
+  };
+  
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
+};
+
+export default AuthProvider;
